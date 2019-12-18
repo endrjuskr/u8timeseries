68f031ca30887d01c6ee3bc7d578ba2ad4269692
MounBen <mounir.benheddi@gmail.com>
2019-12-18 09:50:59 +0100

    fix(docs): update conf.py
    sphinx_rtd_theme needs to be added to requirements.txt to be handled properly by the git-action to build the doc
    replaced by for now (testing phase of the git-action)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+
+# TODO: Fix sphinx_rtd_theme (add in requirements.txt? yes a priori)
+# For now: use default html theme.
+
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
